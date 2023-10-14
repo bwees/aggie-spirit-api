e99ebe39bc229c98759a8b05d09d2b1fa985ac46
bwees <brandonwees@gmail.com>
2023-10-14 13:59:55 -0500

    remove api key

--- examples/example.js
+++ examples/example.js
@@ -1,7 +1,7 @@
 import { getClosestStops } from '../src/gis.js';
 import { getRouteByName, getRoutesByGroup, getRouteStopPredictions, RouteGroup, setORSAPIKey } from '../src/index.js';
 
-setORSAPIKey("5b3ce3597851110001cf62484ad38325dafe4d63baba938c586a2440")
+setORSAPIKey(process.env["ORS_API_KEY"])
 
 getRouteStopPredictions("47").then((predictions) => {
 })
